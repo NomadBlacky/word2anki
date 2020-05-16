2551877c75ba60e5b5e55a1e26a96ed1cf65580d
NomadBlacky <tkadowaki.dev.blackey@gmail.com>
2020-05-16 23:04:24 +0900

    Refactor WeblioClient

--- core/src/word2anki/WeblioClient.scala
+++ core/src/word2anki/WeblioClient.scala
@@ -5,18 +5,39 @@ import java.net.URLEncoder
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.model.Document
+
+import scala.util.Try
 
 class WeblioClient {
+  type ExplanationDoc = Document
+
   private[this] val browser = JsoupBrowser()
 
-  def en2ja(word: String): JapaneseContent = {
+  def en2ja(word: String): Either[WeblioError, JapaneseContent] = {
     val encoded = URLEncoder.encode(word, "utf-8")
-    val doc = browser.get(s"https://ejje.weblio.jp/content/$encoded")
-    val explanation = doc >> text(
-      "#summary > div.summaryM.descriptionWrp > table > tbody > tr > td.content-explanation.ej"
-    )
-    JapaneseContent(explanation)
+    for {
+      doc <- getDocFromUrl(s"https://ejje.weblio.jp/content/$encoded")
+      exp <- extractExplanation(doc)
+    } yield JapaneseContent(exp)
   }
+
+  private def getDocFromUrl(url: String): Either[UnexpectedError, ExplanationDoc] =
+    Try(browser.get(url)).toEither.left.map(UnexpectedError.apply)
+
+  private def extractExplanation(doc: ExplanationDoc): Either[ExplanationNotFoundError.type, String] = {
+    val expOpt = doc >?> text(
+        "#summary > div.summaryM.descriptionWrp > table > tbody > tr > td.content-explanation.ej"
+      )
+    expOpt match {
+      case Some(exp) => Right(exp)
+      case None      => Left(ExplanationNotFoundError)
+    }
+  }
+
+  sealed trait WeblioError
+  case object ExplanationNotFoundError             extends WeblioError
+  case class UnexpectedError(throwable: Throwable) extends WeblioError
 }
 
 case class JapaneseContent(explanation: String)
